15fd82d52b0dd32182d28eb09fd2327af7e79575
ror <ror@rb18-1.(none)>
2012-05-11 20:44:45 +0200

    Add Access-Control-Allow-Origin

--- rb-status.rb
+++ rb-status.rb
@@ -5,6 +5,7 @@ class RbStatus < Sinatra::Base
 
   get '/status' do
     begin
+      response['Access-Control-Allow-Origin'] = ORIGIN_ALLOWED if defined? ORIGIN_ALLOWED
       DB['delete from status;'].first
       before = DB['select id, data from status;'].all.count.to_s
       DB['insert into status(data) values(NOW());'].first
