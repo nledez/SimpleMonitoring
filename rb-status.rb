5904fafeeacf301b4206e173659674c57e733d04
Nicolas Ledez <github@ledez.net>
2012-06-17 23:56:14 +0200

    Convert to pure sequel

--- rb-status.rb
+++ rb-status.rb
@@ -6,10 +6,11 @@ class RbStatus < Sinatra::Base
   get '/status' do
     begin
       response['Access-Control-Allow-Origin'] = ORIGIN_ALLOWED if defined? ORIGIN_ALLOWED
-      DB['delete from status where node = ' + NODE_ID + ';'].first
-      before = DB['select id, data from status where node = ' + NODE_ID + ';'].all.count.to_s
-      DB['insert into status(data, node) values(NOW(, ' + NODE_ID + '));'].first
-      after = DB['select id, data from status, where node = ' + NODE_ID + ';'].all.count.to_s
+      status = DB[:status]
+      status.filter(:node => NODE_ID).delete
+      before = status.filter(:node => NODE_ID).all.count.to_s
+      status.insert(:data => Time.now, :node => NODE_ID)
+      after = status.filter(:node => NODE_ID).all.count.to_s
       if after > before
         "#{VHOST_NAME} OK"
       else
