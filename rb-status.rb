0556d4dc5a9e71611950ead0ace36d90f9145637
Nicolas Ledez <github@ledez.net>
2012-06-18 22:51:18 +0200

    Sleep only if SLEEP_TIME was defined

--- rb-status.rb
+++ rb-status.rb
@@ -6,11 +6,12 @@ class RbStatus < Sinatra::Base
   get '/status' do
     begin
       response['Access-Control-Allow-Origin'] = ORIGIN_ALLOWED if defined? ORIGIN_ALLOWED
+      NODE_ID = 0 unless defined? NODE_ID
       status = DB[:status]
       status.filter(:node => NODE_ID).delete
       before = status.filter(:node => NODE_ID).all.count.to_s
       status.insert(:data => Time.now, :node => NODE_ID)
-      sleep(1)
+      sleep SLEEP_TIME if defined? SLEEP_TIME
       after = status.filter(:node => NODE_ID).all.count.to_s
       if after > before
         "#{VHOST_NAME} OK"
